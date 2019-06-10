bb888bca6e54e0d017adfb3df7e92cddc4393605
Alicia Carrion <alicia.r.carrion@gmail.com>
2019-06-10 12:13:24 -0400

    tests for pqueue

--- gettestie/src/test/java/rocks/zipcode/TestIterator.java
+++ gettestie/src/test/java/rocks/zipcode/TestIterator.java
@@ -1,4 +1,41 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
 public class TestIterator {
+
+//    private LinkedList<String> list;
+//    @Before
+//    public void setUp() throws Exception {
+//        list = new LinkedList<String>();
+//        list.add("Hello world");
+//        list.add("foo");
+//        list.add("bar");
+//        list.add("quimbara");
+//    }
+
+    @Test
+    public void TestIterator1() {
+
+        LinkedList<String> list = new LinkedList<String>();
+        list.add("Hello world");
+        list.add("foo");
+        list.add("bar");
+        list.add("quimbara");
+        Iterator iterator = list.iterator();
+        int count = 1;
+        while (iterator.hasNext()) {count++;}
+        Assert.assertEquals(count, list.size());
+    }
+
+    @Test
+    public void TestIterator2() {
+    }
+    @Test
+    public void TestIterator3() {
+    }
 }
--- gettestie/src/test/java/rocks/zipcode/TestPriorityQueue.java
+++ gettestie/src/test/java/rocks/zipcode/TestPriorityQueue.java
@@ -1,4 +1,48 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.PriorityQueue;
+
 public class TestPriorityQueue {
+    @Test
+    public void TestPque1() {
+        PriorityQueue<String> queue = new PriorityQueue<>();
+        String str = "Hello world";
+        queue.add(str);
+        Assert.assertFalse(queue.isEmpty()); // false
+        Assert.assertTrue(queue.contains(str));
+
+    }
+    @Test
+    public void TestPque2() {
+        PriorityQueue<String> queue = new PriorityQueue<>();
+        String expected = "Hello world";
+        queue.add(expected);
+        queue.add("buffer");
+        String actual = queue.poll();
+        Assert.assertEquals(expected, actual); // true
+    }
+    @Test
+    public void TestPque3() {
+        PriorityQueue<String> queue = new PriorityQueue<>();
+        queue.offer("Hello world");
+        queue.peek();
+        Assert.assertFalse(queue.isEmpty()); // false
+    }
+
+    @Test
+    public void TestPque4() {
+        PriorityQueue<String> queue = new PriorityQueue<>();
+        String str = "Hello world";
+        queue.add(str);
+        queue.add(str);
+        queue.add(str);
+        queue.add(str);
+        queue.remove(str);
+        Assert.assertFalse(queue.isEmpty()); // false
+    }
+
 }
